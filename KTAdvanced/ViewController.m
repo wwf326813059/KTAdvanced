eb945caafcd851e395681ffe6ca5ab63a6edeb21
筑梦师winston <wwf326813059@gmail.com>
2017-12-02 21:37:13 +0800

    test first

--- KTAdvanced/ViewController.m
+++ KTAdvanced/ViewController.m
@@ -16,7 +16,43 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+#pragma mark ----- 12月1日 历史总结
+    /*
+     如何构建一个应用 - 成熟的架构
+     
+     苹果开发账号
+     项目架构
+     登录注册逻辑
+     用户数据存储
+     线程根据用户逻辑调度
+     
+     
+     
+     */
+
+#pragma mark ----- 12月2日学习笔记
+    
+    /*
+     进程
+     线程
+     多线程
+     多线程的优点：适当提高程序的执行效率，适当提高资源利用率
+     多线程的缺点：创建线程占内存，耗时90毫秒。
+     主线程
+     多线程实现方案
+     {
+     pthread 适用于Unix/Linux/Windows ios几乎不用
+     NSThread
+     GCD 封装好的并发技术
+     NSOperation 基于GCD 比GCD多了一些功能
+     }
+     
+     */
+    
+    
+    
+    
 }
 
 
