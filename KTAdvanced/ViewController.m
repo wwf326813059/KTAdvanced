f6af2c5878e24905947dab1ba3901d26c8ee331d
筑梦师winston <wwf326813059@gmail.com>
2018-01-01 23:34:32 +0800

    2018年1月1日

--- KTAdvanced/ViewController.m
+++ KTAdvanced/ViewController.m
@@ -16,42 +16,99 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-#pragma mark ----- 12月1日 历史总结
+    
+    
+#pragma mark -----EveryDay
     /*
-     如何构建一个应用 - 成熟的架构
+     更新
+     晨跑7点-8点
+     金山词霸8点-8点半
+     iOS进阶视频 8点半-10点半
+     做饭 11点半-12点
+     计算机基础书籍 中午1点-中午2点
+     锻炼 2点-2点半
+     iOS进阶视频 2点半-5点半。
      
-     苹果开发账号
-     项目架构
-     登录注册逻辑
-     用户数据存储
-     线程根据用户逻辑调度
      
+     做饭 6点-6点半
+     知乎 6点半-6点50.
+     简书 6点50 -7点10分
+     金山词霸 7点10分 -7点40分
+     w3cSchool 7点40 -8点10分
+     
+     夜跑 8点半 -9点半
+     看美剧 9点半到11点
+     
+     
+     github每日提交
+     每周日 一篇简书
      
      
      */
+    
+#pragma mark -----限时任务
+    /*
+     1.学习python
+     2.学习计算机书籍
+     3.学习iOS11适配
+     4.学习英语
+     5.锻炼身体
+     6.每日关注 热点
+     7.每日关注 应用 iOS
+     
+     
+     */
+    
+#pragma mark -----任务
+    /*
+     1.学习git
+     2.阅读 计算机网络
+     3.阅读 计算机组成原理
+     4.阅读 计算机应用
+     5.学习markdown。
+     
+     
+     历史遗留
+     1.养成良好的书写代码习惯;
+     2.学习hybrid app开发模式;
+     3.学习xmpp即时通讯协议.
+     4,学习JavaScript语法;
+     5.试着理解YYKit;
+     6.学习SVN;
+     7.了解python
+     8.了解sketch软件;
+     9.了解oracle 和跨平台;
+     10.UIBlurEffect UIVisualEffectView
+     11.观看WWDC大会 倒序2016-2012
+     
+     */
 
-#pragma mark ----- 12月2日学习笔记
+#pragma mark ----- iOS进阶学习笔记
+    
+#pragma mark ----- 多线程
     
     /*
-     进程
-     线程
-     多线程
-     多线程的优点：适当提高程序的执行效率，适当提高资源利用率
-     多线程的缺点：创建线程占内存，耗时90毫秒。
-     主线程
-     多线程实现方案
-     {
-     pthread 适用于Unix/Linux/Windows ios几乎不用
-     NSThread
-     GCD 封装好的并发技术
-     NSOperation 基于GCD 比GCD多了一些功能
-     }
-     
      */
     
     
     
+#pragma mark ----- iOS项目开发流程
+    
+#pragma mark ----- app开发流程
+    
+    /*
+     1.新建Xcode项目
+     2.搭建项目架构
+     3.iOS 11 适配
+     
+     iOS 交互手册
+     https://developer.apple.com/ios/human-interface-guidelines/overview/themes/
+     Desgin+code
+     https://designcode.io/swift4-xcode9
+     
+     4.
+     
+     */
     
 }
 
